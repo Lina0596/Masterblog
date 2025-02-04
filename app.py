c072d09c951f3c89ddfc12d1918259fbae180f3a
Lina0596 <lina.dahlhaus@icloud.com>
2025-02-04 22:44:47 +0100

    create delete route

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from crypt import methods
+
 from flask import Flask, render_template, request, redirect, url_for
 import json
 
 
-"""
+
 blog_posts = [
                 {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
                 {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
@@ -11,7 +13,7 @@ blog_posts = [
 json_blog_posts = json.dumps(blog_posts, indent=4)
 with open ("blog_posts.json", "w") as json_file:
     json_file.write(json_blog_posts)
-"""
+
 
 
 app = Flask(__name__)
@@ -32,11 +34,25 @@ def add():
         new_title = request.form.get("title")
         new_content = request.form.get("content")
         blog_posts.append({"id": len(blog_posts) + 1, "author": new_author, "title": new_title, "content": new_content})
-        new_blog_post = json.dumps(blog_posts, indent=4)
+        new_blog_posts = json.dumps(blog_posts, indent=4)
         with open("blog_posts.json", "w") as json_file:
-            json_file.write(new_blog_post)
+            json_file.write(new_blog_posts)
         return redirect(url_for('index'))
     return render_template('add.html')
+
+@app.route('/delete/<int:post_id>', methods=['GET'])
+def delete(post_id):
+    with open("blog_posts.json", "r") as json_file:
+        blog_posts = json.loads(json_file.read())
+    for i in range(len(blog_posts)):
+        if post_id == blog_posts[i]["id"]:
+            del blog_posts[i]
+            new_blog_posts = json.dumps(blog_posts, indent=4)
+            with open("blog_posts.json", "w") as json_file:
+                json_file.write(new_blog_posts)
+            return redirect(url_for('index'))
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
