b4b23c385536e0e43ef7da56322dda811ad4f6cd
Lina0596 <lina.dahlhaus@icloud.com>
2025-02-04 20:37:09 +0100

    create add route and design link

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 
@@ -13,8 +13,8 @@ with open ("blog_posts.json", "w") as json_file:
     json_file.write(json_blog_posts)
 """
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -23,6 +23,20 @@ def index():
         blog_posts = json.loads(json_file.read())
     return render_template('index.html', posts=blog_posts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        with open("blog_posts.json", "r") as json_file:
+            blog_posts = json.loads(json_file.read())
+        new_author = request.form.get("author")
+        new_title = request.form.get("title")
+        new_content = request.form.get("content")
+        blog_posts.append({"id": len(blog_posts) + 1, "author": new_author, "title": new_title, "content": new_content})
+        new_blog_post = json.dumps(blog_posts, indent=4)
+        with open("blog_posts.json", "w") as json_file:
+            json_file.write(new_blog_post)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
